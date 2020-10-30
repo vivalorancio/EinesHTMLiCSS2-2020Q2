9885ea9f38c2aa6b1fa11e3826fd96044f3c803f
vivalorancio <vmateu@gmail.com>
2020-10-30 20:23:59 +0100

    First Commit

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  plugins: ["stylelint-scss"],
+  rules: {
+    "selector-nested-pattern": "^&",
+    indentation: 2,
+    "no-descending-specificity": null,
+    "no-eol-whitespace": null,
+    "declaration-empty-line-before": null,
+    "value-keyword-case": null,
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+  },
+};
